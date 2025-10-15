eb402bdb34d208ec49947720d2150def54a4b0f8
wellinbraga <wellinbraga@gmail.com>
2025-10-15 20:58:37 -0300

    chore: First commit - starting project

---
+++ src/test/java/br/com/stackmob/controleponto/ControlepontoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.stackmob.controleponto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlepontoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
